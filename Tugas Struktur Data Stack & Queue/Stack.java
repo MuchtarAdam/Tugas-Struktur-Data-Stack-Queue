fdb570bd5c2c00d3d668900193d7697106526f43
MuchtarAdam <93690489+MuchtarAdam@users.noreply.github.com>
2022-12-19 16:17:08 +0800

    Add files via upload

---
+++ Tugas Struktur Data Stack & Queue/Stack.java
@@ -0,0 +1,64 @@
+public class Stack {
+private String data[];
+    private int Size;
+    private int Top;
+
+    public Stack (int kapasitas){
+        Size = kapasitas;
+        Top = -1;
+        data = new String [Size];
+
+
+    }
+
+    public boolean isEmpty(){
+        return Top==-1;
+
+    }
+    public boolean isFull(){
+        return Top == Size-1;
+    }
+    public void push (String input){
+        if(isFull()){
+            System.out.println("lemari telah penuh");
+        } else {
+            data[++Top]= input;
+            System.out.println(input+ " dimasukkan ke dalam lemari");
+
+
+        }
+
+    }
+    public void pop () {
+        if(isEmpty()){
+            System.out.println("lemari Kosong");
+        } else {
+
+            String temp = data[Top];
+            data[Top]= data[Top--];
+            System.out.println(temp+ " dikeluarkan dari dalam lemari");
+
+        }
+    }
+
+    public void infoStack(){
+        System.out.println("pakaian yang tersisa dalam lemari : ");
+        for(int i =0; i<=Top;i++){
+
+            System.out.println("*" +data[i]);
+        }
+
+
+        }
+
+    public static void main(String[] args) {
+        Stack s = new Stack(10);
+        s.push("kaos");
+        s.push("cardigan");
+        s.push("celana");
+        s.pop();
+        s.pop();
+        System.out.println("");
+        s.infoStack();
+    }
+}
